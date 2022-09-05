347e1eaae82869b9aec23bd2d6524bebd3f16cd8
Juan Huguet <juan_huguet@epam.com>
2022-09-05 09:58:40 -0500

    DELETE: deletion of unnecessary code

--- api/src/main/java/Main.java
+++ api/src/main/java/Main.java
@@ -2,13 +2,12 @@ import org.jhuguet.utils.Utils;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-            String[] array = {"10", "1", "-112", "22", "21"};
-            for (String item : array) {
-                System.out.print(item + " ");
-            }
-        */
-        System.out.print("10 1 -112 22 21");
-        System.out.println("\nArray items are all positive: " + new Utils().isAllPositiveNumbers("10", "1", "-112", "22", "21"));
+
+        String[] array = {"10", "22", "21", null, ""};
+        for (String item : array) {
+            System.out.print(item + " ");
+        }
+
+        System.out.println("\nArray items are all positive? " + new Utils().isAllPositiveNumbers(array));
     }
 }
